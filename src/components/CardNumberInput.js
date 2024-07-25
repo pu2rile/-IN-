1698568bc21a507c3ebe072b6bd0a8fdf7f3b38c
pu2rile <3o920@naver.com>
2024-07-26 00:20:50 +0900

    [Feat] 카드 번호 입력 시 '-' 추가

--- src/components/CardNumberInput.js
+++ src/components/CardNumberInput.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const CardNumberInput = ({ value, onChange }) => {
+  const formatCardNumber = (number) => {
+    return number.replace(/[^0-9]/g, '').replace(/(\d{4})(?=\d)/g, '$1-');
+  };
+
   const handleChange = (e) => {
-    const inputValue = e.target.value;
-    if (inputValue.length <= 16) {
+    let inputValue = e.target.value;
+    // 숫자와 '-' 이외의 문자는 모두 제거
+    inputValue = inputValue.replace(/[^0-9-]/g, '');
+    // 숫자 4자리마다 '-' 추가
+    inputValue = formatCardNumber(inputValue);
+    // 19자를 초과하지 않도록 제한 (예: 1234-5678-9012-3456)
+    if (inputValue.length <= 19) {
       onChange(inputValue);
     }
   };
