e13f833500694036b87226d34b20897bccfc313d
pu2rile <3o920@naver.com>
2024-07-29 20:18:36 +0900

    [Design] 카드 추가 입력 필드 ui 수정

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,10 +1,4 @@
 import React, { useState } from 'react';
-import CardNumberInput from './CardNumberInput';
-import CardExpiryInput from './CardExpiryInput';
-import CardHolderNameInput from './CardHolderNameInput';
-import CardCVCInput from './CardCVCInput';
-import CardPasswordInput from './CardPasswordInput';
-import RegisterCardButton from './RegisterCardButton';
 
 const CardForm = ({ onAddCard }) => {
   const [cardNumber, setCardNumber] = useState('');
@@ -20,13 +14,60 @@ const CardForm = ({ onAddCard }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardNumberInput value={cardNumber} onChange={setCardNumber} />
-      <CardExpiryInput value={expiry} onChange={setExpiry} />
-      <CardHolderNameInput value={holderName} onChange={setHolderName} />
-      <CardCVCInput value={cvc} onChange={setCVC} />
-      <CardPasswordInput value={password} onChange={setPassword} />
-      <RegisterCardButton />
+    <form onSubmit={handleSubmit} className="card-form">
+      <div className="form-group">
+        <label className="input-label" htmlFor="card-number">카드 번호</label>
+        <input
+          type="text"
+          id="card-number"
+          value={cardNumber}
+          onChange={(e) => setCardNumber(e.target.value)}
+          className="card-number-input"
+        />
+      </div>
+      <div className="form-group">
+        <label className="input-label" htmlFor="expiry">만료일</label>
+        <input
+          type="text"
+          id="expiry"
+          value={expiry}
+          onChange={(e) => setExpiry(e.target.value)}
+          className="expiry-input"
+          placeholder="MM/YY"
+        />
+      </div>
+      <div className="form-group">
+        <label className="input-label" htmlFor="holder-name">카드 소유자 이름</label>
+        <input
+          type="text"
+          id="holder-name"
+          value={holderName}
+          onChange={(e) => setHolderName(e.target.value)}
+          className="holder-name-input"
+          placeholder="카드에 표시된 이름과 동일하게 입력하세요."
+        />
+      </div>
+      <div className="form-group">
+        <label className="input-label" htmlFor="cvc">보안 코드(CVC/CBB)</label>
+        <input
+          type="password"
+          id="cvc"
+          value={cvc}
+          onChange={(e) => setCVC(e.target.value)}
+          className="cvc-input"
+        />
+      </div>
+      <div className="form-group">
+        <label className="input-label" htmlFor="password">카드 비밀번호</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="password-input"
+        />
+      </div>
+      <button className='register-card-button'>카드 등록</button>
     </form>
   );
 };
