7db930b7e26043a9bc8c9ca01838f2d24e93d0b4
pu2rile <3o920@naver.com>
2024-07-29 18:31:55 +0900

    [Style] 카드 소유자 이름 input 수정

--- src/components/CardHolderNameInput.js
+++ src/components/CardHolderNameInput.js
@@ -13,7 +13,7 @@ const CardHolderNameInput = ({ value, onChange }) => {
       type="text"
       value={value}
       onChange={handleChange}
-      placeholder="카드 소유자 이름"
+      placeholder="카드에 표시된 이름과 동일하게 입력하세요."
     />
   );
 };
