51dfbadbaa00e9293d7713bc8498a109bc07a115
pu2rile <3o920@naver.com>
2024-07-29 17:29:28 +0900

    [Feat] 카드 관리 페이지 수정
    카드 추가(입력) 기능 삭제

--- src/components/CardManagement.js
+++ src/components/CardManagement.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import AddCardButton from './AddCardButton'; // 카드 추가 버튼 컴포넌트
-import CardForm from './CardForm'; // 카드 입력 폼 컴포넌트
 import RegisteredCard from './RegisteredCard'; // 등록된 카드 컴포넌트
 
 const CardManagement = () => {
@@ -29,7 +28,6 @@ const CardManagement = () => {
           <AddCardButton onClick={() => setAddingCard(true)} />
         </div>
       )}
-      {addingCard && <CardForm onAddCard={handleAddCard} />}
     </div>
   );
 };
