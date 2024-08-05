7347a7b45df9f188470283685edb4e3143452577
pu2rile <3o920@naver.com>
2024-08-05 22:28:27 +0900

    [Build] App.test.js 수정

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
-import { act } from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
-  act(() => {
-    render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    );
-  });
-  const linkElement = screen.getByText(/learn react/i);
+test('renders product title', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const linkElement = screen.getByText(/신발 상품 목록/i);
   expect(linkElement).toBeInTheDocument();
 });
